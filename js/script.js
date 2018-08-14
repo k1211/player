59469a7a4b5afb5666b6e43175e33a728704cba7
kasiejro <kgarbulewska@gmail.com>
2018-08-14 12:22:47 +0200

    playlist view initialization

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var song1 = {"title" : "Still don't know", "author" : "Icona Pop", "time" : "3:16"};
+var song2 = {"title" : "Still don't know", "author" : "Icona Pop", "time" : "3:16"};
+var song3 = {"title" : "Still don't know", "author" : "Icona Pop", "time" : "3:16"};
+var song4 = {"title" : "Still don't know", "author" : "Icona Pop", "time" : "3:16"};
+var song5 = {"title" : "Still don't know", "author" : "Icona Pop", "time" : "3:16"};
+
+var songs = [song1, song2, song3, song4, song5];
