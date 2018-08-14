a3de7f64930dab4d4bb0ae14a9ada1fa25bae02e
kasiejro <kgarbulewska@gmail.com>
2018-08-14 20:48:29 +0200

    player initialization

--- js/script.js
+++ js/script.js
@@ -1,19 +1,74 @@
-var song1 = {"id": "1", "title" : "Still don't know", "author" : "Icona Pop", "time" : "3:16"};
-var song2 = {"id": "2", "title" : "Girlfriend", "author" : "Icona Pop", "time" : "3:16"};
-var song3 = {"id": "3", "title" : "Still don't know", "author" : "Icona Pop", "time" : "3:16"};
-var song4 = {"id": "4", "title" : "Still don't know", "author" : "Icona Pop", "time" : "3:16"};
-var song5 = {"id": "5", "title" : "Still don't know", "author" : "Icona Pop", "time" : "3:16"};
+var song1 = {"id": "0", "title" : "Still don't know", "author" : "Icona Pop", "time" : "3:16"};
+var song2 = {"id": "1", "title" : "Girlfriend", "author" : "Icona Pop", "time" : "3:16"};
+var song3 = {"id": "2", "title" : "Still don't know", "author" : "Icona Pop", "time" : "3:16"};
+var song4 = {"id": "3", "title" : "Still don't know", "author" : "Icona Pop", "time" : "3:16"};
+var song5 = {"id": "4", "title" : "Still don't know", "author" : "Icona Pop", "time" : "3:16"};
 
 var songs = [song1, song2, song3, song4, song5];
 
+$( document ).ready(init);
 
-$( document ).ready(generateSongs);
+
+function init() {
+	generateSongs();
+	let songElements = document.getElementsByClassName("song");
+	
+	for(let i = 0; i < songElements.length; i++) {
+		songElements[i].onclick = goToPlayer;
+	}
+}
+
+
+function showPlayer(e) {
+	player = document.getElementById("player");
+	playlist = document.getElementById("playlist");
+	console.log("player" + player);
+		console.log("playlist" + playlist);
+
+
+	player.removeAttribute('hidden');
+	playlist.setAttribute('hidden', true);
+}
+
+
+function showPlaylist(e) {
+	player = document.getElementById("player");
+	playlist = document.getElementById("playlist");
+
+	player.setAttribute('hidden', true);
+	playlist.removeAttribute('hidden');
+}
+
+
+function getSongNumber(clickedElement) {
+	let condition = true;
+	while(condition) {
+		let parent = clickedElement.parentElement;
+		if (parent.className === "song") {
+			condition = false;
+			return parent.id;
+		}
+		clickedElement = parent;
+	}
+}
+
+
+function initPlayer(songNumber) {
+	document.getElementById("piosenka").innerHTML = "Piosenka numer " + songNumber;
+}
+
+function goToPlayer(e) {
+	let songNumber = getSongNumber(e.target);
+	initPlayer(songNumber);
+	showPlayer();
+}
+
 
 function generateSongs() {
     for(let i = 0; i < songs.length; i++) {
     	$("#song-section")
 			.append(
-				$('<div class="song">')
+				$('<div class="song" id="' + songs[i].id+ '">')
 				.append(
 					$('<div class="song-details">')
 					.append($('<div class="song-time-and-author">')
@@ -26,12 +81,6 @@ function generateSongs() {
 					.append($('<a><i class="fas fa-share-alt"></i></a>'))
 				)
 			);
-			// 		.append('<div class="song-time-and-author"> ')
-			// 			.append('<p id="song-time">' + songs[i].time + ' &nbsp;|&nbsp; </p> ')
-			// 			.append('<p id="song-author">' + songs[i].author + '</p>  ')
-			// 		.append('</div')
-			// 	.append('</div>')
-			// 	.append('<div class="social">')
 	}
 }
 
